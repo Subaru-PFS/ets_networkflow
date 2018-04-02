3f44bd8d7fa9a2619dd25329446304f774749453
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-04-02 12:38:45 +0200

    add some plotting

--- demo.py
+++ demo.py
@@ -2,6 +2,90 @@ import pyETS
 import libETS.data_model as dm
 import numpy as np
 
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+from matplotlib import patches as mpatches
+from matplotlib import collections
+
+
+class Limits(object):
+    def __init__(self):
+        self._xmin = 1e20
+        self._xmax = -1e20
+        self._ymin = 1e20
+        self._ymax = -1e20
+
+    def include (self,x,y,r=0.):
+        self._xmin = min(self._xmin,x-r)
+        self._xmax = max(self._xmax,x+r)
+        self._ymin = min(self._ymin,y-r)
+        self._ymax = max(self._ymax,y+r)
+
+    @property
+    def xmin(self):
+        return self._xmin
+
+    @property
+    def xmax(self):
+        return self._xmax
+
+    @property
+    def ymin(self):
+        return self._ymin
+
+    @property
+    def ymax(self):
+        return self._ymax
+
+def plot_cobra(c, patches, limits, facecolor='none', edgecolor='black', plot_dot=False):
+    x, y, r = c.center.real, c.center.imag, c.innerLinkLength + c.outerLinkLength
+    limits.include(x, y, r)
+    circle = mpatches.Circle((x, y), r, facecolor=facecolor, edgecolor=edgecolor, lw=1.)
+    patches.append(circle)
+    if plot_dot:
+        x, y, r = c.dotcenter.real, c.dotcenter.imag, c.rdot
+        circle = mpatches.Circle((x, y), r, facecolor=facecolor, edgecolor=edgecolor, lw=1.)
+        patches.append(circle)
+
+def plot(cbr):
+    def plotNodes(ax, nodes, limits):
+        # targets
+        patches = []
+        for i, c in enumerate(nodes):
+            plot_cobra(c, patches, limits, plot_dot=True)
+
+        collection = collections.PatchCollection(patches, match_original=True)
+        ax.add_collection(collection)
+
+    limits=Limits()
+    fig = plt.figure(figsize=[15, 15])
+    ax = plt.subplot(111)  # note we must use plt.subplots, not plt.subplot
+    plotNodes(ax, cbr, limits)
+    ax.set_ylim([limits.ymin, limits.ymax])
+    ax.set_xlim([limits.xmin, limits.xmax])
+    plt.show()
+
+def plot_assignment(cobras,targets,res):
+    xmin, xmax, ymin, ymax = 1e3,-1e3,1e3,-1e3
+    fig = plt.figure(figsize=[15, 15])
+    ax = plt.subplot(111)  # note we must use plt.subplots, not plt.subplot
+    patches = []
+    limits=Limits()
+    for tidx, cidx in res.items():
+        c = cobras[cidx]
+        t = targets[tidx]
+        color = "red" if isinstance(t, dm.ScienceTarget) else "green"
+        plot_cobra(c, patches, limits, edgecolor=color)
+        x, y = c.center.real, c.center.imag
+        tx, ty = t.position.real, t.position.imag
+        line = mlines.Line2D([x, tx], [y, ty], lw=1, color= color)
+        ax.add_line(line)
+    collection = collections.PatchCollection(patches, match_original=True)
+    ax.add_collection(collection)
+    ax.set_ylim([limits.ymin, limits.ymax])
+    ax.set_xlim([limits.xmin, limits.xmax])
+    plt.show()
+
 # define locations of the input files
 catalog_path = "/home/martin/codes/ets_fiber_assigner/pfs_target_list"
 fscience_targets = catalog_path+"/pfs_preliminary_target_cosmology.dat"
@@ -18,8 +102,10 @@ tgt += dm.readCalibrationFromFile(fsky_pos, "sky")
 cobras = dm.getFullFocalPlane()
 
 # build reduced Cobra list to speed up calculation
-cobras = [c for c in cobras if abs(c.center) < 200]
+cobras = [c for c in cobras if abs(c.center) < 100]
 
+
+#plot(cobras)
 # point the telescope at the center of all science targets
 raTel, decTel = dm.telescopeRaDecFromFile(fscience_targets)
 posang = 0.
@@ -34,8 +120,10 @@ classdict["sci_P4"] = {"nonObservationCost": 70, "partialObservationCost": 1e9}
 classdict["sci_P5"] = {"nonObservationCost": 60, "partialObservationCost": 1e9}
 classdict["sci_P6"] = {"nonObservationCost": 50, "partialObservationCost": 1e9}
 classdict["sci_P7"] = {"nonObservationCost": 40, "partialObservationCost": 1e9}
-classdict["sky"] = {"numRequired": 1}
+classdict["sky"] = {"numRequired": 2}
 classdict["cal"] = {"numRequired": 1}
 
 res = telescope.observeWithNetflow(tgt, classdict, 3, 300.)
-print(res)
+for vis in res:
+    plot_assignment(cobras, tgt, vis)
+
